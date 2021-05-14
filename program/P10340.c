bef43c7bf944ed3f1a06931f758e654242ce9525
jxes993409 <jxes993409@gmail.com>
2021-05-14 21:29:33 +0800

    Add File

---
+++ program/P10340.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#define max 10000
+
+int main()
+{
+    int i,flag,j;
+    char s[max],t[max];
+    while(scanf("%s %s",s,t)!=EOF)
+    {
+        flag=0,j=0;
+        for(i=0;i<strlen(s);i++)
+        {
+            for(j=j;j<strlen(t);j++) //只能往後檢查，若找到符合字元便跳出迴圈
+            {
+                if(s[i]==t[j])
+                {
+                    flag++;
+                    break;
+                }
+            }
+        }
+        if(flag==strlen(s)) printf("Yes\n");
+        else printf("No\n");
+    }
+    return 0;
+}
